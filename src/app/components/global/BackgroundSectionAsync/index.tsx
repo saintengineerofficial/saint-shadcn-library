fd14047b2db45052e70cf0817f12b576088b88a1
caizhihao <caizhihao@iyinguo.com>
2025-12-05 21:27:36 +0800

    feat: global组件

--- src/app/components/global/BackgroundSectionAsync/index.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from 'next/dynamic';
-
-const BackgroundSectionAsync = dynamic(() => import('./BackgroundSection'), {
-  ssr: false,
-})
-
-export default BackgroundSectionAsync;
